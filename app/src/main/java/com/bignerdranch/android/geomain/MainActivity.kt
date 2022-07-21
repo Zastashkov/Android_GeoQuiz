d56f17511c1cb1e7d0773134b58d2d3e40f7443c
Daniel Zastashkov <zast2020@mail.ru>
2022-07-21 20:37:54 +0300

    Toast above

--- app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geomain/MainActivity.kt
@@ -2,6 +2,7 @@ package com.bignerdranch.android.geomain
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
@@ -18,11 +19,15 @@ class MainActivity : AppCompatActivity() {
         falseButton = findViewById(R.id.false_button);
 
         trueButton.setOnClickListener { view: View ->
-            Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT).show()
+            val toast = Toast.makeText(this, R.string.correct_toast, Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.TOP, 0, 220)
+            toast.show()
         }
 
         falseButton.setOnClickListener { view: View ->
-            Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+            val toast = Toast.makeText(this, R.string.incorrect_toast, Toast.LENGTH_SHORT)
+            toast.setGravity(Gravity.TOP, 0, 220)
+            toast.show()
         }
     }
 }
